892cb4d7d4af15aa0a522acd4978006ec2c9fd5c
Hackron-Dev <ivanmyasnikov004@gmail.com>
2022-11-23 18:14:10 +0300

    added checks (dont work yet)

--- main.py
+++ main.py
@@ -17,21 +17,68 @@ def printBoard(board):
     print(board[6] + " |", board[7] + " |", board[8])
 
 
-#printBoard(board)
+# printBoard(board)
 
 
 # take pleyer input
 def takePlayerInput(board):
     inp = int(input("Enter a number 1-9: "))
     if 1 <= inp <= 9 and board[inp - 1] == "_":
-        board[inp-1] = currentPlayer
+        board[inp - 1] = currentPlayer
     else:
         print("You cant go there!")
 
+
 # check for win or tie
+def checkHorizontal(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != "_":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "_":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "_":
+        winner = board[6]
+        return True
+    else:
+        return False
 
 
+def checkRow(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "_":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "_":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "_":
+        winner = board[2]
+        return True
+    else:
+        return False
+
+
+def checkDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "_":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "_":
+        winner = board[2]
+        return True
+    else:
+        return False
+
+def checkTie(board):
+    global gameRunning
+    if "_" not in board:
+        printBoard(board)
+        print("It's a tie!")
+        gameRunning = False
+
+# run game
 while gameRunning:
     printBoard(board)
     takePlayerInput(board)
-
